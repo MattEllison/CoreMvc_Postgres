5b55ed4d37d4cb7319d865fc007ba5a688812871
Matthew Ellison <matthewcellison@gmail.com>
2020-03-30 21:25:03 -0500

    firstcommit

---
+++ DAL/Database.cs
@@ -0,0 +1,24 @@
+using System;
+using Npgsql;
+using Microsoft.AspNetCore.Mvc;
+namespace postgress.Database
+{
+    public class Database
+    {
+        public static void Connect()
+        {
+
+            var cs = "Host=localhost;Username=temp;Password=temp;Database=tempdb";
+            //var c = Configuration.GetSection("DatabaseConfig")["PostgresSQL"]);
+            using var con = new NpgsqlConnection(cs);
+            con.Open();
+
+            var sql = "SELECT version()";
+
+            using var cmd = new NpgsqlCommand(sql, con);
+
+            var version = cmd.ExecuteScalar().ToString();
+        }
+    }
+}
+
